f43c2fc45610c3f8693e0676a5f5696422073f3c
heyitsk <kushagradpr2005@gmail.com>
2025-01-03 01:26:56 +0530

    server created,  nodemon  installed and scripts updated

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+const express = require("express")
+
+const app = express()
+//this creates an express js application 
+
+
+
+app.use("/test",(req,res)=>{
+    res.send("hello from test page")
+})
+
+app.use("/hello",(req,res)=>{
+    res.send("hello from hello page")
+})
+app.use("/",(req,res)=>{
+    res.send("hello from the server")
+})
+
+app.listen(3000,()=>{
+    console.log("server succesffuly listening on port 300");
+    
+})
