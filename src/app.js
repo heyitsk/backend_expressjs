47f41047b7c29195369c094e3830a42341a0e14f
heyitsk <kushagradpr2005@gmail.com>
2025-01-04 12:58:49 +0530

    http request and routing

--- src/app.js
+++ src/app.js
@@ -5,17 +5,36 @@ const app = express()
 
 
 
-app.use("/test",(req,res)=>{
-    res.send("hello from test page")
-})
+// app.use("/test",(req,res)=>{
+//     res.send("hello from test page")
+// })
 
-app.use("/hello",(req,res)=>{
-    res.send("hello from hello page")
-})
-app.use("/",(req,res)=>{
-    res.send("hello from the server")
-})
+// app.use("/hello",(req,res)=>{
+//     res.send("hello from hello page")
+// })
+// app.use("/",(req,res)=>{
+//     res.send("hello from the server")
+// })
 
+app.get("/user/:userId/:name/:age",(req,res)=>{
+    // console.log(req.query);
+    console.log(req.params);
+    
+    
+    res.send({
+        "firstname":"kushagra",
+        "lastname":"agarwal"
+    })
+
+})
+app.post("/user",(req,res)=>{
+    //saving data to database logic 
+    res.send("data saved successfully")
+})
+app.delete("/user",(req,res)=>{
+    //deleting user logic
+    res.send("user deleted successfully")
+})
 app.listen(3000,()=>{
     console.log("server succesffuly listening on port 300");
     
