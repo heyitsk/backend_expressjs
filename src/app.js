3f75b912b99065603460c64461bec4a4513d18de
heyitsk <kushagradpr2005@gmail.com>
2025-01-19 12:35:41 +0530

    data updation using findbyidandupdate

--- src/app.js
+++ src/app.js
@@ -180,6 +180,35 @@ app.get("/feed",async (req,res)=>{
 
 })
 
+//delete user by _id 
+app.delete("/user",async(req,res)=>{
+    const userId = req.body._id
+    try{
+
+        const user = await User.findByIdAndDelete({_id:userId})
+        res.send("user deleted")
+    }
+    catch(err){
+        res.send("some error ocurred cannot delete the user")
+    }
+})
+
+//update data fo the user using pathc
+app.patch("/user",async(req,res)=>{
+    const data = req.body
+    const userId= req.body._id
+    try{
+
+         const userInfo = await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after"})
+        console.log(userInfo);
+        
+        res.send("data updates succesfully")
+    }
+    catch(err){
+        res.status(400).send("something went wrong")
+    }
+}) //this does not update the _id field bcz its not present in the schema of the model. SO anything let's suppose you try to add another fields while updating which are not in the schema it'll ignore all of them 
+
 connectCluster()
     .then(()=>
         {
